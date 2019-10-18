74f5e3b3e785e30368e92db8b5b8ce4e0a90c0ee
Luanne Barbosa <luannebarbosa@gmail.com>
2019-10-18 14:45:05 -0300

    Codigos para cropar rostos, 'main' apenas para teste

---
+++ main.py
@@ -0,0 +1,9 @@
+# Import libraries
+import os
+import cv2
+import numpy as np
+from data_generator import Data_generator
+from face_extractor import Face_extractor
+
+Data_generator()
+Face_extractor()
